22232b73b82ab35599a0b9e298b620cb7aa8c808
Stanislav Sazonov <slavk11@yandex.ru>
2023-04-09 14:17:27 +0300

    initialized array with type Answer

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -8,17 +8,33 @@
 import UIKit
 
 final class ResultViewController: UIViewController {
-
+    
+    @IBOutlet var totalResult: UILabel!
+    
+    var answers: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationItem.setHidesBackButton(true, animated: true)
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
     
+    func getFinalResult() {
+        var animals: [Animal: Int] = [:]
+        let totalAnswers = answers.map { $0.animal}
+        
+        for answer in totalAnswers {
+            animals[answer] = animals[answer]
+        }
+    }
+    
     deinit {
         print("\(type(of: self)) has been deallocated")
     }
+    
+   
 }
